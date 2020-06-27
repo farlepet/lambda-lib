3a685b073c98532b6a3f78913c77345ea8e62e8f
Peter Farley <far.peter1@gmail.com>
2020-06-26 20:24:12 -0500

    Fixed array initialization in `wait`.

--- src/sys/wait/wait.c
+++ src/sys/wait/wait.c
@@ -4,7 +4,7 @@
 #include <lambda/sys/syscalls.h>
 
 pid_t wait(int *stat_loc) {
-    uint32_t args[1] = (uint32_t)stat_loc;
+    uint32_t args[1] = { (uint32_t)stat_loc };
     call_syscall(SYSCALL_WAIT, args);
     return (pid_t)args[0];
 }
